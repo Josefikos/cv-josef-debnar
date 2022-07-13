15585c9354f8e1a36c5ec61deb7c678b62f1d34f
Josef Debnár <josefdebnar7@gmail.com>
2022-07-13 13:49:50 +0200

    fix build
    absolute path for assets in index.html
    add vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+const { resolve } = require('path');
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'public/index.html'),
+            }
+        }
+    }
+});
