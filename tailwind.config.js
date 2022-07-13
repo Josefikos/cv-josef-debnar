d778035734aa9ab88b9a87b10a8b243d6c8e29a6
Josef Debnár <josefdebnar7@gmail.com>
2022-07-13 13:37:47 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './public/**/*.{css,js,html}',
+    ],
+    theme: {
+        extend: {
+
+        },
+    }, plugins: [
+
+    ],
+}
